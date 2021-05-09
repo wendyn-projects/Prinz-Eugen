c1d829d0465cfc9496072dca9d1957be2ae6dd7b
vaclav nemec <vaclav.nemec70@gmail.com>
2021-05-09 15:48:22 +0200

    Unused imports removed

--- bot/src/bot/commands/administration/add/AdminRole.ts
+++ bot/src/bot/commands/administration/add/AdminRole.ts
@@ -2,7 +2,7 @@ import * as Discord from 'discord.js'
 import * as Interface from '../../../../interface'
 import ArrayFormatter from '../../../../formatting/Array'
 import GuildConfig from '../../../../server/config/GuildConfig'
-import { CommandNameOption, Response } from '../../interface'
+import { Response } from '../../interface'
 
 export default class extends Interface.ValueAction<Response> {
 
@@ -26,7 +26,7 @@ export default class extends Interface.ValueAction<Response> {
             (this.message.mentions.roles.size > 1?
                 (ArrayFormatter.createList(this.message.mentions.roles.map((role: Discord.Role) => role.toString())) + ' roles were'): 
                 (this.message.mentions.roles.first() + ' role was')
-            )+
+            ) +
             ' given administrative permissions by ' + 
             this.message.member
         );
